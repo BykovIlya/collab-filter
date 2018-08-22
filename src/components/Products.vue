d738e0c31bc4bbe2d6155263ab1894b24e1b63a5
ilyabykov <bikovbiv@gmail.com>
2018-08-22 16:09:39 +0300

    first

---
+++ src/components/Products.vue
@@ -0,0 +1,14 @@
+<template>
+        <h1>Products</h1>
+</template>
+
+<script>
+export default {
+    name: 'Products',
+    data () {
+        return {
+            msg: 'Welcome to SmartStore'
+        }
+    }
+}
+</script>
---
+++ src/components/Users.vue
@@ -0,0 +1,10 @@
+<template>
+    <b-container>
+        <h1>Users</h1>
+    </b-container>
+    </div>
+</template>
+
+<script>
+
+</script>
