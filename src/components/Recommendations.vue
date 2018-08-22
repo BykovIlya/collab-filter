4f260353a30b516ae8a8667e1b6d183202f49988
ilyabykov <bikovbiv@gmail.com>
2018-08-22 17:17:54 +0300

    first

--- src/components/Recommendations.vue
+++ src/components/Recommendations.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>
-    <h1>Recommendations</h1>
-  </div>
+  <b-container fluid>
+    <b-row class="my-1">
+      <b-col sm="2"><label for="input-user">Enter the user:</label></b-col>
+      <b-col sm="4">
+        <b-form-input id="input-user" type="text" placeholder="ready..."></b-form-input>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,10 +1,61 @@
 <template>
-    <b-container>
-        <h1>Users</h1>
+    <b-container fluid>
+        <b-card border-variant="">
+            <template slot="header">
+                <b-row>
+                    <b-col sm="5">
+                        <h4 class="card-title">Table of users</h4>
+                    </b-col>
+                    <b-col sm="7">
+                        <div class ="float-right">
+                            <b-btn  variant="info" @click="showModalExport">export from .csv</b-btn>
+                        </div>
+                    </b-col>
+                </b-row>
+            </template>
+            <b-table id="products"
+                     striped
+                     show-empty
+                     :busy.sync="isBusy"
+                     :fields="fields"
+                     :current-page="currentPage"
+                     :per-page="perPage"
+                     :sort-by.sync="sortBy"
+                     :sort-desc.sync="sortDesc"
+            >
+            </b-table>
+            <b-row>
+                <b-col sm="12">
+                    <b-pagination align="right" :total-rows="totalRows" :per-page="perPage" v-model="currentPage" class="my-0" />
+                </b-col>
+            </b-row>
+        </b-card>
     </b-container>
-    </div>
 </template>
 
 <script>
+    export default {
+        name: 'Users',
+        data () {
+            return {
+                formUrl: 'users',
+                fields: [
+                    {
+                        key: 'id',
+                    },
+                    {
+                        key: 'name',
+                        label: this.$t('name'),
+                    },
+                ],
+                currentPage: 1,
+                perPage: 15,
+                isBusy: false,
+                totalRows:0,
+                sortBy: null,
+                sortDesc: false,
+            }
+        }
+    }
 
 </script>
